80c4bd26cd1e452ad0ac4da9523d7a497f61f4a4
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-04-10 02:04:18 +0400

    add_changes

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ container.classList.add("container");
 document.body.prepend(container);
 
 const input = document.createElement("input");
+input.placeholder = "Bir mebleg daxil edin...";
 const button = document.createElement("button");
 button.id = "btn_axtar";
 button.textContent = "Axtar";
 container.append(input, button, mycontainer);
 
-let miqdar2;
+let miqdar2 = [];
 btn_axtar.addEventListener("click", () => {
   mycontainer.innerHTML = "";
   miqdar2 = setCountMoney(input.value);
   fillMoney(miqdar2);
-  input.value = "";
 });
 
+let pullar = [500, 200, 100, 50, 20, 10, 5, 1];
 function setCountMoney(input_value) {
-  let pullar = [500, 200, 100, 50, 20, 10, 5, 1];
   let miqdar = [];
   let k;
   let sum = input_value;
@@ -41,19 +41,29 @@ function setCountMoney(input_value) {
 }
 
 function fillMoney(miqdar2) {
-  let pullar = [500, 200, 100, 50, 20, 10, 5, 1];
-
   miqdar2.map((value, index) => {
-    if (value !== 0) {
+    if (value > 0 && value <= 5) {
       const board = document.createElement("div");
       mycontainer.append(board);
       for (let j = 1; j <= value; j++) {
         board.classList.add("board");
         const img = document.createElement("img");
         img.src = `./images/${pullar[index]}.jpg`;
-        img.style.left = `${10 * j}px`;
+        img.style.left = `${50 * j}px`;
         board.append(img);
       }
+      const label = document.createElement("p");
+      label.classList.add("label");
+      label.innerHTML = `${value} X`;
+      board.append(label);
+    } else if (value > 5) {
+      const board = document.createElement("div");
+      board.classList.add("board");
+      mycontainer.append(board);
+      const img = document.createElement("img");
+      img.src = `./images/${pullar[index]}.jpg`;
+      img.style.left = `${50}px`;
+      board.append(img);
       const label = document.createElement("p");
       label.classList.add("label");
       label.innerHTML = `${value} X`;
